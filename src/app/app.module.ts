2dcebeb1bc0245c440c079eeae531f639ad6bf9d
Matthew Bauer <matthewbauer@Matthews-iMac.local>
2017-01-18 21:45:08 -0500

    Lesson 2 branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './ui/search-form/search-form.component';
+import { HelloWorldComponent } from './ui/hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent,
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule,
